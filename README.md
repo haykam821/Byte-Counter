09026946dfcc1dc144656a1d973ebb8ee4310c1c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-20 15:33:36 +0000

    Deploy haykam821/Byte-Counter to github.com/haykam821/Byte-Counter.git:gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# Byte-Counter
-A simple web app for counting bytes/characters in a string.
--- src/index.js
+++
@@ -1,7 +0,0 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-
-const App = require("./components/app.js");
-
-const appContainer = document.getElementById("app");
-ReactDOM.render(React.createElement(App), appContainer);
